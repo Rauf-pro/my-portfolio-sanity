28361c33f65440f38a411b4fbf0f7fd2da4aa3f5
rrauf8178 <rrauf8178@gmail.com>
2024-05-05 00:58:09 +0400

    add: update all npm packages

--- frontend_react/src/components/NavigationDots.js
+++ frontend_react/src/components/NavigationDots.js
@@ -2,9 +2,11 @@ import React from "react";
 
 const NavigationDots = ({ active }) => (
   <div className="app__navigation">
-    {["home", "about", "work", "skills", "testimonial", "contact"].map((item, index) => (
+    {["home", "about", "work", "skills", "testimonial", "contact"].map(
+      (item, index) => (
         <a
           href={`#${item}`}
+          rel="noreferrer"
           key={item + index}
           className="app__navigation-dot"
           style={active === item ? { backgroundColor: "#313BAC" } : {}}
