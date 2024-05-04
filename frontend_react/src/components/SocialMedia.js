04bfcf8350e3b8f2ac9a85e42ecb7a0d96df07e5
rrauf8178 <rrauf8178@gmail.com>
2024-05-04 22:23:59 +0400

    fix: href rel reffe

--- frontend_react/src/components/SocialMedia.js
+++ frontend_react/src/components/SocialMedia.js
@@ -1,22 +1,21 @@
 import React from "react";
 import { BsInstagram } from "react-icons/bs";
 import { FaFacebookF, FaLinkedinIn } from "react-icons/fa";
-import { RiTwitterXLine } from "react-icons/ri";
 
 const SocialMedia = () => (
   <div className="app__social">
     <a
       href="https://www.linkedin.com/in/rauf-rzayev-b12827219"
-      target="_blank"
+      target="_blank rel=noreferrer"
     >
       <FaLinkedinIn />
     </a>
   
 
-    <a href="https://www.facebook.com/rauf.rzayev.904" target="_blank">
+    <a href="https://www.facebook.com/rauf.rzayev.904" target="_blank" rel="noreferrer">
       <FaFacebookF />
     </a>
-    <a href="https://www.instagram.com/rauf.zr/" target="_blank">
+    <a href="https://www.instagram.com/rauf.zr/" target="_blank" rel="noreferrer">
       <BsInstagram />
     </a>
   </div>
