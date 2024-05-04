4d47807927aed72798e1f57dfed873a2a3f053a8
rrauf8178 <rrauf8178@gmail.com>
2024-05-04 21:18:30 +0400

    add: social media links

--- frontend_react/src/components/SocialMedia.js
+++ frontend_react/src/components/SocialMedia.js
@@ -1,18 +1,24 @@
-import React from 'react';
-import { BsTwitter, BsInstagram } from 'react-icons/bs';
-import { FaFacebookF } from 'react-icons/fa';
+import React from "react";
+import { BsInstagram } from "react-icons/bs";
+import { FaFacebookF, FaLinkedinIn } from "react-icons/fa";
+import { RiTwitterXLine } from "react-icons/ri";
 
 const SocialMedia = () => (
   <div className="app__social">
-    <div>
-      <BsTwitter />
-    </div>
-    <div>
+    <a
+      href="https://www.linkedin.com/in/rauf-rzayev-b12827219"
+      target="_blank"
+    >
+      <FaLinkedinIn />
+    </a>
+  
+
+    <a href="https://www.facebook.com/rauf.rzayev.904" target="_blank">
       <FaFacebookF />
-    </div>
-    <div>
+    </a>
+    <a href="https://www.instagram.com/rauf.zr/" target="_blank">
       <BsInstagram />
-    </div>
+    </a>
   </div>
 );
 
